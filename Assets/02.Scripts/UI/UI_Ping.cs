6fef20dd9345bf02f64449c48d535b0d7b05d9e8
kencoor9 <kencoor9@gmail.com>
2025-07-28 17:19:57 +0900

    이형근 : PlayerPing 핵심 로직 구현

--- Assets/02.Scripts/UI/UI_Ping.cs
+++ Assets/02.Scripts/UI/UI_Ping.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class UI_Ping : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject _arm;
+    [SerializeField] private Transform _playerTransform;
+    private Camera _camera;
+
+    private Vector3 _playerScreenPoint;
+    private Vector3 _pingScreenPoint;
+
+    private void Awake()
     {
-        
+        _camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_playerTransform == null)
+        {
+            GameObject enemy = GameObject.FindWithTag("Enemy");
+            if (enemy == null)
+            {
+                return;
+            }
+
+            _playerTransform = enemy.transform;
+        }
+
+        _playerScreenPoint = _camera.WorldToScreenPoint(_playerTransform.position);
+
+        // if (!CheckPlayerIsInCamera())
+        // {
+        //     gameObject.SetActive(false);
+        // }
+        // else
+        // {
+            gameObject.SetActive(true);
+            
+            _pingScreenPoint = _playerScreenPoint;
+            _pingScreenPoint.x = _pingScreenPoint.x < 0 ? 100 : _pingScreenPoint.x;
+            _pingScreenPoint.x = _pingScreenPoint.x > Screen.width ? Screen.width - 100 : _pingScreenPoint.x;
+            _pingScreenPoint.y = _pingScreenPoint.y < 0 ? 100 : _pingScreenPoint.y;
+            _pingScreenPoint.y = _pingScreenPoint.y > Screen.height ? Screen.height - 100 : _pingScreenPoint.y;
+            transform.position = _camera.ScreenToWorldPoint(_pingScreenPoint);
+
+            Vector3 dir = (_playerTransform.position - transform.position).normalized;
+            _arm.transform.localRotation = Quaternion.LookRotation(transform.forward, -dir);
+        // }
+    }
+
+    private bool CheckPlayerIsInCamera()
+    {
+        return _playerScreenPoint.y > 0 && _playerScreenPoint.x > 0 && _playerScreenPoint.x < Screen.width && _playerScreenPoint.y < Screen.height;
     }
 }
