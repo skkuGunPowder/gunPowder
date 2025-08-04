9dfb6e535cc6dda8194d551435f136c29c56b7ea
kencoor <kencoor9@gmail.com>
2025-08-04 18:30:32 +0900

    이형근 : 대포 기믹 추가

--- Assets/02.Scripts/Gimmick/Cannon/Cannon.cs
+++ Assets/02.Scripts/Gimmick/Cannon/Cannon.cs
@@ -1,42 +1,67 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class Cannon : MonoBehaviour
 {
     [SerializeField] private GameObject _barrel;
 
-    [SerializeField] private float _fireForece = 10f;
+    [SerializeField] private float _fireForce = 100f;
     [SerializeField] private float _fireDegree = 20f;
 
     private Rigidbody2D _firedProjectile;
     bool _isLoaded = false;
 
-
     private void Fire()
     {
-        _firedProjectile.AddForce(_barrel.transform.right * _fireForece, ForceMode2D.Impulse);
+        _firedProjectile.AddForce(_barrel.transform.right * _fireForce, ForceMode2D.Impulse);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (_isLoaded)
         {
             return;
         }
 
-        _firedProjectile = collision.gameObject.GetComponent<Rigidbody2D>();
+        _firedProjectile = collision.attachedRigidbody;
+
+        StartCoroutine(FireRoutine(_firedProjectile));
+    }
+
+    private IEnumerator FireRoutine(Rigidbody2D target)
+    {
         _isLoaded = true;
 
-        _barrel.transform.DORotate(new Vector3(0, 0, _fireDegree), 0.5f)
+        target.linearVelocity = Vector2.zero;
+        target.bodyType = RigidbodyType2D.Kinematic;
+        target.transform.DOMove(_barrel.transform.position, 0.3f).SetEase(Ease.InQuad)
         .OnComplete(() =>
         {
-            DOVirtual.DelayedCall(0.3f, () => Fire());
-            _barrel.transform.DORotate(new Vector3(0, 0, 0), 0.5f)
-            .OnComplete(() =>
-            {
-                _isLoaded = false;
-                _firedProjectile = null;
-            });
+            target.gameObject.SetActive(false);  
         });
+        
+        transform.DOScale(Vector3.one * 1.2f, 0.15f)
+        .SetLoops(2, LoopType.Yoyo);
+        yield return new WaitForSeconds(0.3f);
+
+        _barrel.transform.DORotate(new Vector3(0, 0, _fireDegree), 0.5f).SetEase(Ease.InOutSine);
+        yield return new WaitForSeconds(0.5f);
+
+        yield return new WaitForSeconds(0.3f);
+
+        transform.DOScale(Vector3.one * 1.2f, 0.15f)
+        .SetLoops(2, LoopType.Yoyo);
+
+        target.gameObject.SetActive(true);
+        target.bodyType = RigidbodyType2D.Dynamic;
+        Vector2 fireDir = _barrel.transform.right.normalized;
+        target.AddForce(fireDir * _fireForce, ForceMode2D.Impulse);
+
+        _barrel.transform.DORotate(Vector3.zero, 0.5f).SetEase(Ease.InOutSine);
+        yield return new WaitForSeconds(0.5f);
+
+        _isLoaded = false;
     }
 }
