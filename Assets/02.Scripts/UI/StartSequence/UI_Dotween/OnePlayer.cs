713c2ba432532e7fdf9dcd41dc1b8e9e20a36b9f
SeongIl037 <kerect@naver.com>
2025-08-06 10:42:50 +0900

    양성일 : 솔로로 테스트시 연출 스킵

---
+++ Assets/02.Scripts/UI/StartSequence/UI_Dotween/OnePlayer.cs
@@ -0,0 +1,17 @@
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using UnityEngine;
+
+public class OnePlayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnEnable()
+    {
+        Hashtable hash = new Hashtable()
+        {
+            {EProperties.IsLoad.ToString(), true}
+        };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+    }
+    
+}
