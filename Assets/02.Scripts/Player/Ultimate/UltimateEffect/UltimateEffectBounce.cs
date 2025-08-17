9529eb348597e57f655ca0b9c49a4b46563276aa
SeongIl037 <kerect@naver.com>
2025-08-17 23:52:57 +0900

    양성일 : 전체에게 궁극기 연출 뜨도록 적용

---
+++ Assets/02.Scripts/Player/Ultimate/UltimateEffect/UltimateEffectBounce.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class UltimateEffectBounce :  UltimateEffectBase
+{
+}
---
+++ Assets/02.Scripts/Player/Ultimate/UltimateProductionSlot.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class UltimateProductionSlot : MonoBehaviour
+{
+    public List<UltimateEffectBase> EffectList;
+    
+    public List<GameObject> UltimateEffectList;
+    [Header("궁극기 연출 시간")]
+    public float UltimateTime = 1f;
+    [Header("궁극기 애니메이션 오브젝트 관련")]
+    public RectTransform UltimateEffectUp;
+    public RectTransform UltimateEffectDown;
+    public Image UpBackGround;
+    public Image DownBackGround;
+    public float UltimateEffectSpeed = 1f;
+    public Ease UltimateEffectInEase = Ease.Linear;
+    public Ease UltimateEffectOutEase = Ease.Linear;
+    public Vector2 UltimateEndPosition;
+    public Vector2 UltimateEndPosition2;
+    
+    [Header("위치 초기화")]
+    public Vector2 UltimateOriginPosition;
+    public Vector2 UltimateOriginPosition2;
+    
+    [Header("컬러")]
+    public Color32 MyTeam;
+    public Color32 OtherTeam;
+    
+    public void Play(string bomb, bool isMyTeam)
+    {
+        
+        UltimateEffectUp.gameObject.SetActive(true);
+        UltimateEffectDown.gameObject.SetActive(true);
+        
+        BackGroundColorChange(isMyTeam);
+        
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(UltimateEffectUp.DOAnchorPos(UltimateEndPosition, UltimateEffectSpeed).SetEase(UltimateEffectInEase));
+        sequence.Join(UltimateEffectDown.DOAnchorPos(UltimateEndPosition2, UltimateEffectSpeed).SetEase(UltimateEffectInEase));
+        sequence.JoinCallback(()=>BombEffectOn(bomb));
+        sequence.AppendInterval(UltimateTime);
+        sequence.Append(UltimateEffectUp.DOAnchorPos(UltimateOriginPosition, UltimateEffectSpeed).SetEase(UltimateEffectOutEase));
+        sequence.Join(UltimateEffectDown.DOAnchorPos(UltimateOriginPosition2, UltimateEffectSpeed).SetEase(UltimateEffectOutEase));
+        sequence.OnComplete(() =>
+        {
+            BombEffectOff(bomb);
+            
+            UltimateEffectUp.gameObject.SetActive(false);
+            UltimateEffectDown.gameObject.SetActive(false);
+            
+            this.gameObject.SetActive(false);
+        });
+    }
+
+    private void BackGroundColorChange(bool isMyTeam)
+    {
+        if (isMyTeam)
+        {
+            UpBackGround.color = MyTeam;
+            DownBackGround.color = MyTeam;
+        }
+        else
+        {
+            UpBackGround.color = OtherTeam;
+            DownBackGround.color = OtherTeam;
+        }
+    }
+    private void BombEffectOn(string bomb)
+    {
+        Debug.Log($"{bomb}");
+        foreach (UltimateEffectBase effect in EffectList)
+        {
+            if (effect.BombName == bomb)
+            {
+                effect.Play();
+                break;
+            }
+        }
+    }
+    private void BombEffectOff(string bomb)
+    {
+        Debug.Log($"{bomb}");
+        foreach (UltimateEffectBase effect in EffectList)
+        {
+            if (effect.BombName == bomb)
+            {
+                effect.Stop();
+                break;
+            }
+        }
+    }
+    private void OnDisable()
+    {
+        DOTween.Kill(this);
+        
+        UltimateEffectUp.anchoredPosition = UltimateOriginPosition;
+        UltimateEffectDown.anchoredPosition = UltimateOriginPosition2;
+    }
+}
