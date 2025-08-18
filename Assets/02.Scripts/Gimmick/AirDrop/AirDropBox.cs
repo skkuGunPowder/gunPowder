411485d044da6979ed6d0b3fbc589fa8043551b3
kencoor <kencoor9@gmail.com>
2025-08-18 13:27:32 +0900

    이형근 : 공중 보급 버그 수정

--- Assets/02.Scripts/Gimmick/AirDrop/AirDropBox.cs
+++ Assets/02.Scripts/Gimmick/AirDrop/AirDropBox.cs
@@ -1,19 +1,50 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Photon.Pun;
 
 public class AirDropBox : MonoBehaviour
 {
-    public ParticleSystem LandingVFX;
+    public ParticleSystem LandingVFXPrefab;
+    public ParticleSystem DropWarningVFXPrefab;
     public List<AirDropItemBase> DropItemList;
+    public LayerMask layerMask;
     private bool _isInAir = true;
+
+    private GameObject _dropWarningVFX;
+
+    private void Awake()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 40f, layerMask);
+        if (hit.collider == null)
+        {
+            return;
+        }
+
+        Debug.LogWarning($"Hitpoint [{hit.point}]");
+        Debug.DrawLine(transform.position, hit.point, Color.red, 10f);
+
+        _dropWarningVFX = PhotonNetwork.Instantiate(DropWarningVFXPrefab.name, hit.point, Quaternion.identity);
+    }
 
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("TileMap") && _isInAir)
+        if (other.gameObject.CompareTag("TileMap") || other.gameObject.CompareTag("OneWayPlatform"))
         {
+            if (!_isInAir)
+            {
+                return;
+            }
             _isInAir = false;
-            ParticleSystem landingVFX = Instantiate(LandingVFX, other.contacts[0].point, Quaternion.identity);
+
+            Destroy(_dropWarningVFX);
+
+            ParticleSystem landingVFX = Instantiate(LandingVFXPrefab, other.contacts[0].point, Quaternion.identity);
+            VFXPool.Instance.Get(LandingVFXPrefab.name);
+            if (!_isInAir)
+        {
+            return;
+        }
             landingVFX.Play();
             return;
         }
--- Assets/02.Scripts/Gimmick/AirDrop/AirDropJet.cs
+++ Assets/02.Scripts/Gimmick/AirDrop/AirDropJet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class AirDropJet : Jet
 {
     [SerializeField] private Transform _dropTransfrom;
-    [SerializeField] private ParticleSystem _dropWarningVFX;
+    
  
     private PhotonView _photonView;
 
@@ -20,13 +20,12 @@ public class AirDropJet : Jet
             return;
         }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 40f,LayerMask.NameToLayer("Platform"));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 40f, LayerMask.NameToLayer("Platform"));
         if (hit.collider == null)
         {
             return;
         }
 
-        PhotonNetwork.Instantiate(_dropWarningVFX.name, hit.point, Quaternion.identity);
         PhotonNetwork.Instantiate(_dropItemList[0].name, _dropTransfrom.position, Quaternion.identity);
     }
 }
