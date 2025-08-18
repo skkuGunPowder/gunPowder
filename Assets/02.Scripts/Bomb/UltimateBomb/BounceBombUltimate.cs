e741668337617f188601274e7fc23b1d5eced47a
kencoor <kencoor9@gmail.com>
2025-08-18 12:44:30 +0900

    이형근 : 바운스봄 궁극기 2개 나가기, 기본폭발 시 카메라 흔들림 제거

--- Assets/02.Scripts/Bomb/UltimateBomb/BounceBombUltimate.cs
+++ Assets/02.Scripts/Bomb/UltimateBomb/BounceBombUltimate.cs
@@ -18,17 +18,18 @@ public class BounceBombUltimate : Ultimate
 
     public override void ExcuteUltimate()
     {
-        if (_owner.PlayerStat.FacingDirection != 1)
+        _owner.transform.DOShakePosition(1f, 0.1f, 50, 30, false, false).OnComplete(() =>
         {
-            _fireDirection *= -1;
-        }
-
-        _owner.transform.DOShakePosition(1f, 0.1f, 50, 30, false, false).OnComplete(()=>
-        {
-            GameObject bounceBombOBJ = PhotonNetwork.Instantiate(UltimateBouncePrefab.name, _owner.transform.position, Quaternion.identity);
-            UltimateBounceBomb bounceBomb = bounceBombOBJ.GetComponent<UltimateBounceBomb>();
-            bounceBomb.PhotonView.RPC(nameof(bounceBomb.SetOwner), RpcTarget.All, _owner.PhotonView.ViewID);
-            bounceBomb.PhotonView.RPC(nameof(bounceBomb.ThrowBomb), RpcTarget.All, _fireDirection, _owner.transform.up, _owner.transform.forward);    
+            Fire(_fireDirection);
+            Fire(-_fireDirection);
         });
+    }
+
+    private void Fire(Vector3 fireDir)
+    {
+        GameObject bounceBombOBJ = PhotonNetwork.Instantiate(UltimateBouncePrefab.name, _owner.transform.position, Quaternion.identity);
+        UltimateBounceBomb bounceBomb = bounceBombOBJ.GetComponent<UltimateBounceBomb>();
+        bounceBomb.PhotonView.RPC(nameof(bounceBomb.SetOwner), RpcTarget.All, _owner.PhotonView.ViewID);
+        bounceBomb.PhotonView.RPC(nameof(bounceBomb.ThrowBomb), RpcTarget.All, fireDir, _owner.transform.up, _owner.transform.forward);
     }
 }
