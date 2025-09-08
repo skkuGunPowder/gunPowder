f4c7c1a66eaab2641d6a05434f89f99c97fed28d
SeongIl037 <kerect@naver.com>
2025-09-08 11:43:49 +0900

    양성일 : 피격 효과

--- Assets/02.Scripts/UI/HitScreen.cs
+++ Assets/02.Scripts/UI/HitScreen.cs
@@ -7,11 +7,13 @@ using DG.Tweening;
 public class HitScreen : MonoBehaviour
 {
     public Image HitScreenImage;
-    public int Value = 80;
-    public int MaxValue = 230;
+    public float Value = 0.3f;
+    public int MaxValue = 1;
     public float FadeSpeed = 0.5f;
     public float FadeInSpeed = 0.3f;
     public Color ScreenColor = Color.white;
+    private Sequence currentSequence;
+
     private void Awake()
     {
         EventManager.Instance.OnHitScreen += PlayHitScreen;
@@ -20,26 +22,23 @@ public class HitScreen : MonoBehaviour
 
     private void PlayHitScreen()
     {
-        DOTween.Kill(this);
-
-        int currentValue = (int)HitScreenImage.color.a;
-        currentValue = Mathf.Min(currentValue + Value, MaxValue);
-        Color newColor = new Color(ScreenColor.r, ScreenColor.g, ScreenColor.b, currentValue);
-        FadeIn(newColor);
-    }
-
-    private void FadeIn(Color newColor)
-    {
-        HitScreenImage.DOColor(newColor, FadeInSpeed).OnComplete(() =>
+        // 이전 시퀀스가 있다면 완료 콜백을 제거하고 Kill
+        if (currentSequence != null && currentSequence.IsActive())
         {
-            FadeOut();   
-        });
+            currentSequence.Kill(false); // false 파라미터로 현재 값을 유지
+        }
+
+        float currentValue = HitScreenImage.color.a;
+        currentValue = Mathf.Min(currentValue + Value, MaxValue);
+        Debug.Log(currentValue);
+        Play(currentValue);
     }
 
-    
-    private void FadeOut()
+    private void Play(float fade)
     {
-        HitScreenImage.DOColor(ScreenColor, FadeSpeed);
+        currentSequence = DOTween.Sequence();
+        currentSequence.Append(HitScreenImage.DOFade(fade, FadeInSpeed));
+        currentSequence.Append(HitScreenImage.DOFade(0, FadeSpeed).SetEase(Ease.InCirc));
     }
     private void OnDestroy()
     {
