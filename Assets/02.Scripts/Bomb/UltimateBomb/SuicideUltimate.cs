09dc6b40fd7bbd2edce604a29fd86682ec999d15
kencoor <kencoor9@gmail.com>
2025-09-11 01:19:13 +0900

    이형근 : 자폭폭탄 궁극기 기능 완료

--- Assets/02.Scripts/Bomb/UltimateBomb/SuicideUltimate.cs
+++ Assets/02.Scripts/Bomb/UltimateBomb/SuicideUltimate.cs
@@ -1,6 +1,28 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class SuicideUltimate : Ultimate
 {
-    
+    private const string ID = "BO0019";
+
+    [Header("References")]
+    [SerializeField] GameObject UltimateSuicidePrefab;
+
+    public override void Init()
+    {
+        _ownerBombID = "BO0018";
+
+        _bombStat = ItemDatabase.Instance.GetStat<BombStat>(ID);
+    }
+
+    public override void ExcuteUltimate()
+    {
+        if (_owner.PhotonView.IsMine)
+        {
+            GameObject suicideBombOBJ = PhotonNetwork.Instantiate(UltimateSuicidePrefab.name, _owner.transform.position, Quaternion.identity);
+            UltimateSuicide suicideBomb = suicideBombOBJ.GetComponent<UltimateSuicide>();
+            suicideBomb.PhotonView.RPC(nameof(suicideBomb.SetOwner), RpcTarget.All, _owner.PhotonView.ViewID);
+            suicideBomb.PhotonView.RPC(nameof(suicideBomb.PlaceBomb), RpcTarget.All, Vector3.zero, Vector3.zero, Vector3.zero);
+        }
+    }
 }
