1d41c361bdd739fd77de38239ae09ea316df4c29
SeongIl037 <kerect@naver.com>
2025-09-27 00:47:54 +0900

    양성일 : 룸 최신화 수정

--- Assets/02.Scripts/Photon/UI_Popup/Lobby/UI_RoomSearchPopup.cs
+++ Assets/02.Scripts/Photon/UI_Popup/Lobby/UI_RoomSearchPopup.cs
@@ -13,11 +13,6 @@ public class UI_RoomSearchPopup : UI_Popup
     private int _currentPage = 1;
     private int _maxPage = 1;
     
-    // private void Awake()
-    // {
-    //     LobbyManager.Instance.OnDataChanged += Refresh;
-    // }
-    
     private void OnEnable()
     {
         EventManager.Instance.OnRoomListUpdate += Refresh;
@@ -28,9 +23,8 @@ public class UI_RoomSearchPopup : UI_Popup
     // 방이 보일 수 있을만큼만 보여준다 (최대 8개) => refresh할 것들 : 현재 있는 방의 개수, 방 페이지,
     public void Refresh()
     { 
-        List<RoomInfo> roomInfoList = LobbyManager.Instance.RoomInfoList;
+        List<RoomInfo> roomInfoList = PhotonServerManager.Instance.RoomInfoList;
         
-        Debug.Log(" roomcount"+roomInfoList.Count);;
         // 방이 0개인 경우
         if (roomInfoList == null || roomInfoList.Count == 0)
         {
