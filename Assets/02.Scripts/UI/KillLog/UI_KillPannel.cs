3149fa374a721fe76da8781bcb8d0b7a7252dcf8
SeongIl037 <kerect@naver.com>
2025-11-04 00:23:43 +0900

    양성일 : 킬 패널 완성

--- Assets/02.Scripts/UI/KillLog/UI_KillPannel.cs
+++ Assets/02.Scripts/UI/KillLog/UI_KillPannel.cs
@@ -1,26 +1,53 @@
 using System;
 using TMPro;
 using UnityEngine;
-
+using DG.Tweening;
 public class UI_KillPannel : MonoBehaviour
 {
     public TextMeshProUGUI KillPannelText;
-    [SerializeField] private float _time = 1f;
-    private float _timer;
+    public TextMeshProUGUI KillPannelBG;
+    [SerializeField] private float _fadeTime = 1f;
+    [SerializeField] private float _intervalTime = 1f;
+    private Color32 _bgColor;
+    private Color32 _textColor;
     
+    private void Awake()
+    {
+        _bgColor = KillPannelBG.color;
+        _textColor = KillPannelText.color;
+    }
     public void Refresh(string playerNickname)
     {
-        KillPannelText.text = playerNickname;
+        
+        DOTween.Kill(this);
+        
+        // 중간에 킬 되었을 때, 처음 시작
+        KillPannelBG.text = $"{playerNickname}를 처치하였습니다.";
+        KillPannelText.text = $"<color=red>{playerNickname}</color>를 처치하였습니다.";
+        
+        KillPannelBG.color = _bgColor;
+        KillPannelText.color = _textColor;
+        
+        Play();
     }
 
-    private void Update()
+    private void Play()
     {
-        _timer += Time.deltaTime;
+        // 페이드 아웃
+        SoundManager.Instance.PlayLocalSound("KillLocal_2", transform);
+        Sequence sequence = DOTween.Sequence();
+        sequence.AppendInterval(_intervalTime);
+        sequence.Append(KillPannelBG.DOFade(0, _fadeTime));
+        sequence.Join(KillPannelText.DOFade(0, _fadeTime));
         
-        if (_timer >= _time)
-        {
-            _timer = 0;
-            this.gameObject.SetActive(false);
-        }
+        
+        
     }
+
+    private void OnDisable()
+    {
+        KillPannelBG.color = _bgColor;
+        KillPannelText.color = _textColor;
+    }
+
 }
