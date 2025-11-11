e0d0dfb3fb3d309636e4c282c36fd905f19c190c
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-11-11 18:10:24 +0900

    허정범 폭탄대쉬 폭탄 2개 생성되던 버그 수정

--- Assets/02.Scripts/Player/State/PlayerFallState.cs
+++ Assets/02.Scripts/Player/State/PlayerFallState.cs
@@ -605,10 +605,11 @@ public class PlayerFallState : PlayerBaseState
         Vector3 position = _owner.GetExplosionSpawnPoint().position;
         GameObject prefab = PhotonNetwork.Instantiate("BasicBomb", position, Quaternion.identity);
         
-        if (prefab.TryGetComponent(out Bomb bomb))
+        if (prefab.TryGetComponent(out Bomb bomb) && _owner.PhotonView.IsMine)
         {
             bomb.PhotonView.RPC(nameof(bomb.SetOwner), RpcTarget.All, _owner.PhotonView.ViewID);
             bomb.PhotonView.RPC(nameof(bomb.Explode), RpcTarget.All);
+            PhotonNetwork.Destroy(prefab);
             _explosionOverrideTimer = EXPLOSION_OVERRIDE_DURATION;
         }
     }
