fac4edd88290da0f1601ccb3c1b75e791c4f5acb
kencoor <kencoor9@gmail.com>
2025-11-20 21:40:34 +0900

    이형근 : 스토브 SDK 연동

---
+++ Assets/02.Scripts/Stove/STOVEManager.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using UnityEngine;
+using System.Text;
+
+using static Stove.PCSDK.Base;
+using static Stove.PCSDK.IAP;
+using NUnit;
+
+
+public class STOVEManager : MonoBehaviour
+{
+    private bool _isInitialized;
+
+    private float _runCallbackInternval = 1.0f;
+    private Coroutine _runCallbackCoroutine;
+
+    private StovePCInitializeParam _initParam;
+
+    private static STOVEManager _instance;
+    private static object _lockObject = new object();
+
+    public static STOVEManager Instance
+    {
+        get
+        {
+            lock (_lockObject)
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType<STOVEManager>();
+
+                    if (_instance == null)
+                    {
+                        _instance = new GameObject().AddComponent<STOVEManager>();
+                        _instance.name = "STOVEPCSDK3Manager";
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        _initParam = new StovePCInitializeParam
+        {
+            environment = "LIVE",
+            gameId = EnvLoader.Get("GAME_ID"),
+            applicationKey = EnvLoader.Get("APPLICATION_KEY")
+        };
+
+        Base_RestartAppIfNecessaryAsync(_initParam, 60_000, (CallbackResult callbackResult, bool restartAppIfNecessary) =>
+        {
+            PrintCallbackResult(callbackResult);
+
+            if (restartAppIfNecessary)
+            {
+                // 런처를 통해 게임을 실행하지 않았므로 게임종료 처리를 진행합니다. 
+                Debug.LogError("Please run the game through the stove launcher.");
+            }
+            else
+            {
+                // 런처를 통해 게임이 실행되었으므로 초기화를 진행합니다.
+                Debug.Log("Success to run through stove launcher.");
+                Initialize(EnvLoader.Get("SHOP_KEY"));
+            }
+        });
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (_isInitialized)
+        {
+           UnInitialize();
+        }
+    }
+
+    private IEnumerator RunCallbackCoroutine()
+    {
+        var wfs = new WaitForSeconds(_runCallbackInternval);
+
+        while (true)
+        {        		
+            Base_RunCallback();
+
+            yield return wfs;
+        }
+    }
+
+    public void StartRunCallbackLoop()
+    {
+        if (_runCallbackCoroutine == null)
+        {
+            Debug.Log("Start RunCallbackLoop");
+
+            _runCallbackCoroutine = StartCoroutine(RunCallbackCoroutine());
+        }
+    }
+
+    public void StopRunCallbackLoop()
+    {
+        if (_runCallbackCoroutine != null)
+        {
+            Debug.Log("Stop RunCallbackLoop");
+
+            StopCoroutine(_runCallbackCoroutine);
+            _runCallbackCoroutine = null;
+        }
+    }
+
+    public void PrintResult(Stove.PCSDK.Base.Result r)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("# Result");
+        sb.AppendLine($" - Result.sdkName : {r.sdkName}");
+        sb.AppendLine($" - Result.methodCode : {r.methodCode}");
+        sb.AppendLine($" - Result.resultCode : {r.resultCode}");
+        sb.AppendLine($" - Result.exceptionMessage : {r.exceptionMessage}");
+
+        Debug.Log(sb.ToString());
+    }
+
+    public void PrintCallbackResult(CallbackResult cr)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("# CallbackResult");
+        sb.AppendLine($" - CallbackResult.Result.sdkName : {cr.result.sdkName}");
+        sb.AppendLine($" - CallbackResult.Result.methodCode : {cr.result.methodCode}");
+        sb.AppendLine($" - CallbackResult.Result.resultCode : {cr.result.resultCode}");
+        sb.AppendLine($" - CallbackResult.Result.exceptionMessage : {cr.result.exceptionMessage}");
+        sb.AppendLine($" - CallbackResult.message : {cr.errorMessage}");
+        sb.AppendLine($" - CallbackResult.externalError : {cr.externalError}");
+
+        Debug.Log(sb.ToString());
+    }
+
+    public void Initialize(string shopKey)
+    {
+        StartRunCallbackLoop();
+
+        Base_Initialize(_initParam, (CallbackResult callbackResult) =>
+        {
+            PrintCallbackResult(callbackResult);
+
+            if (callbackResult.result.IsSuccessful())
+            {
+                Stove.PCSDK.Base.Result result = default;
+
+                Debug.Log("Success to initialize Base SDK");
+                // result = IAP_Initialize(shopKey);
+                PrintResult(result);
+
+                _isInitialized = true;
+            }
+            else
+            {
+                Debug.LogError("Fail to initialize Base SDK");
+            }
+        });
+    }
+
+    public void UnInitialize()
+    {
+        Stove.PCSDK.Base.Result result;
+
+        StopRunCallbackLoop();
+
+        result = Base_UnInitialize();
+        PrintResult(result);
+
+        result = IAP_UnInitialize();
+        PrintResult(result);
+
+        _isInitialized = false;
+    }
+}
