788beb9a8338c87db849905fcd63e957f5d00dbd
SeongIl037 <kerect@naver.com>
2025-11-24 22:04:46 +0900

    양성일 : 버튼 중복 호출 막기

--- Assets/02.Scripts/Photon/Lobby/UI/UI_QuickStart.cs
+++ Assets/02.Scripts/Photon/Lobby/UI/UI_QuickStart.cs
@@ -1,17 +1,29 @@
-using ExitGames.Client.Photon;
+using System.Collections;
 using Photon.Pun;
 using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
 public class UI_QuickStart : MonoBehaviour
 {
+    [SerializeField] private float _clickInterval = 0.5f;
+    private bool _isClickInterval = false; // 쿨타임
+    
     // 개인으로 만들어진 방 랜덤으로 들어가기
     public void OnClickQuickJoinRoom()
     {
+        if (_isClickInterval)
+        {
+            return;
+        }
+
+        StartCoroutine(ClickInterval_Coroutine());
+
         Hashtable hash = new Hashtable()
         {
             {ERoomProperties.IsLocked.ToString(), false}   
         };
-        PhotonNetwork.JoinRandomRoom(hash, 0);
         
+        PhotonNetwork.JoinRandomRoom(hash, 0);
     }
 
     // 팀으로 만들어진 방 중 랜덤으로 들어가기
@@ -19,5 +31,16 @@ public class UI_QuickStart : MonoBehaviour
     {
         // LobbyManager의 party라는 변수가 null이 아니라면 이 함수를 호출한다.
         // 
+    }
+    private IEnumerator ClickInterval_Coroutine()
+    {
+        _isClickInterval = true;
+        yield return new WaitForSeconds(_clickInterval);
+        _isClickInterval = false;
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(ClickInterval_Coroutine());
     }
 }
--- Assets/02.Scripts/UI/SceneChanger.cs
+++ Assets/02.Scripts/UI/SceneChanger.cs
@@ -1,11 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using MaskTransitions;
 public class SceneChanger : MonoBehaviour
 {
     public ESceneList Scene;
-
+    [SerializeField] private float _clickInterval = 0.5f;
+    private bool _isClickInterval = false; // 쿨타임
     public void OnClickSceneChange()
     {
+        if (_isClickInterval)
+        {
+            return;
+        }
+        
+        _isClickInterval = true; // 즉시 쿨타임 활성화
+
+        StartCoroutine(ClickInterval_Coroutine());
+        
         if (Scene == ESceneList.Tutorial)
         {
             Tutorial();
@@ -19,5 +30,16 @@ public class SceneChanger : MonoBehaviour
     {
         Debug.Log("OnclickTutorial");
         PhotonServerManager.Instance.TutorialMode();
+    }
+    
+    private IEnumerator ClickInterval_Coroutine()
+    {
+        yield return new WaitForSeconds(_clickInterval);
+        _isClickInterval = false;
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(ClickInterval_Coroutine());
     }
 }
