0fbb8aede1aa18a4776eb26d95ea59cc3e6678d0
namunamunamu <kencoor9@gmail.com>
2026-04-09 00:31:01 +0900

    이형근 : 카트리지 뒤끝 차트 데이터 연동 및 프리펩화

---
+++ Assets/02.Scripts/Cartridge/CartridgeRepository.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using BackEnd;
+using LitJson;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class CartridgeRepository
+{
+    public Task<Dictionary<string, CartridgeData>> LoadCartridgeDataAsync()
+    {
+        TaskCompletionSource<Dictionary<string, CartridgeData>> loadCompletionSource = new TaskCompletionSource<Dictionary<string, CartridgeData>>();
+        Dictionary<string, CartridgeData> dataDict = new Dictionary<string, CartridgeData>();
+
+        Backend.Chart.GetChartListV2(result =>
+        {
+            if (!result.IsSuccess())
+            {
+                Debug.LogError($"카트리지 데이터 불러오기 실패: {result.GetMessage()}");
+                loadCompletionSource.TrySetResult(dataDict);
+                return;
+            }
+
+            foreach (JsonData chart in result.FlattenRows())
+            {
+                if (chart["chartName"].ToString() != "Cartridge")
+                {
+                    continue;
+                }
+
+                var chartContents = Backend.Chart.GetChartContents(chart["selectedChartFileId"].ToString());
+                if (!chartContents.IsSuccess())
+                {
+                    Debug.LogError($"카트리지 데이터 불러오기 실패: {chartContents.GetMessage()}");
+                    continue;
+                }
+
+                foreach (JsonData cartridgeInfo in chartContents.FlattenRows())
+                {
+                    CartridgeData cartridgeData = new CartridgeData(cartridgeInfo);
+                    dataDict[cartridgeData.ID] = cartridgeData;
+                }
+            }
+
+            loadCompletionSource.TrySetResult(dataDict);
+        });
+
+        return loadCompletionSource.Task;
+    }
+}
