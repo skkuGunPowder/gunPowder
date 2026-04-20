3d8b83f0e4c7eb02897a3799c08e3b4273352c1d
namunamunamu <kencoor9@gmail.com>
2026-04-20 23:44:00 +0900

    이형근 : 뒤끝 Dev 폴더 차트 대응

--- Assets/02.Scripts/Buff/BuffDomains/BuffRepository.cs
+++ Assets/02.Scripts/Buff/BuffDomains/BuffRepository.cs
@@ -6,6 +6,13 @@ using System;
 
 public class BuffRepository
 {
+#if DEV_MODE
+    //Dev 폴더
+    private const int ITEM_DATA_FOLDER_ID = 3124;
+#else
+    //Build 폴더
+    private const int ITEM_DATA_FOLDER_ID = 3125;
+#endif
     public event Action<Dictionary<string, BuffStat>> OnStatLoaded;
 
     public BuffRepository()
@@ -17,14 +24,14 @@ public class BuffRepository
     {
         Dictionary<string, BuffStat> statDict = new Dictionary<string, BuffStat>();
 
-        Backend.Chart.GetChartListV2(result =>
+        Backend.Chart.GetChartListByFolderV2(ITEM_DATA_FOLDER_ID, result =>
         {
             if (!result.IsSuccess())
             {
-                Debug.LogError($"버프 데이터 불러오기 실패: {result.GetMessage()}");
+                Debug.LogError($"[BUFF] DEV 차트 불러오기 실패: {result.GetMessage()}");
                 return;
             }
-
+            
             foreach (JsonData chart in result.FlattenRows())
             {
                 if (chart["chartName"].ToString() != "Buff")
